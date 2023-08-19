e3147e0bcd15af5eeb91e5d4b76bb04c620afe47
Rainer Simon <rainer@rainersimon.io>
2023-08-19 13:24:17 +0200

    maplibre plumbing for mixed GeoJSON layers

--- src/peripleo/maplibre/Layers/StaticDataLayer/StaticDataLayer.tsx
+++ src/peripleo/maplibre/Layers/StaticDataLayer/StaticDataLayer.tsx
@@ -21,32 +21,54 @@ export const StaticDataLayer = (props: StaticDataLayerProps) => {
 
   useEffect(() => {
     const points = 
-      fc(props.data?.features.filter(f => f.geometry.type === 'Point'));
+      fc(props.data?.features.filter(f => f.geometry?.type === 'Point'));
 
     const shapes =
-      fc(props.data?.features.filter(f => f.geometry.type !== 'Point'));
+      fc(props.data?.features.filter(f => f.geometry?.type !== 'Point'));
 
-    const sourceId = `${props.id}-source`;
+    const pointSourceId = `${props.id}-pt-source`;
+    const pointLayerId = `${props.id}-pt`;
 
-    map.addSource(sourceId, {
+    const shapeSourceId = `${props.id}-sh-source`;
+    const shapeLayerId = `${props.id}-sh`;
+
+    map.addSource(pointSourceId, {
       type: 'geojson',
-      data: props.data
+      data: points
+    });
+
+    map.addSource(shapeSourceId, {
+      type: 'geojson',
+      data: shapes
     });
 
     // @ts-ignore
     map.addLayer({
       // ...props.style,
       'type': 'circle',
-      id: props.id,
-      source: sourceId,
+      id: pointLayerId,
+      source: pointSourceId,
+      metadata: {
+        interactive: true,
+      }
+    });
+
+    map.addLayer({
+      // ...props.style,
+      'type': 'fill',
+      id: shapeLayerId,
+      source: shapeSourceId,
       metadata: {
         interactive: true,
       }
     });
 
     return () => {
-      map.removeLayer(props.id);
-      map.removeSource(sourceId);
+      map.removeLayer(pointLayerId);
+      map.removeLayer(shapeLayerId);
+      
+      map.removeSource(pointSourceId);
+      map.removeSource(shapeSourceId);
     }
   }, []);
 
