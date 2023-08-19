6ad79fbce149b665a52fcc534e7564b41c69390a
Rainer Simon <rainer@rainersimon.io>
2023-08-19 09:39:42 +0200

    Made panel resizable

--- src/peripleo/layout/DraggablePanel/DraggablePanel.tsx
+++ src/peripleo/layout/DraggablePanel/DraggablePanel.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, useRef } from 'react';
-import { useDraggable } from '@neodrag/react';
+import { ReactNode } from 'react';
+import Draggable from 'react-draggable';
+import { ResizableBox } from 'react-resizable';
 import { CSSDimension } from '../Types';
 
 import './DraggablePanel.css';
@@ -10,43 +11,38 @@ export interface DraggablePanelProps {
 
   className?: string;
   
-  height?: CSSDimension | number;
+  height?: number;
 
   left?: number;
 
   top?: number; 
 
-  width?: CSSDimension | number;
-
-  onDragStart?(): void;
-
-  onDragEnd?(): void;
+  width?: number;
 
 }
 
 export const DraggablePanel = (props: DraggablePanelProps) => {
 
-  const { children, className, onDragStart, onDragEnd } = props;
+  const { children, className } = props;
 
-  const height = props.height || 500;
-
-  const left = props.left || 20;
+  const left = `${props.left || 20}px`;
   
-  const top = props.left || 20;
+  const top = `${props.left || 20}px`;
 
   const width = props.width || 400;
 
-  const ref = useRef<HTMLDivElement>(null);
-  
-  useDraggable(ref, { onDragStart, onDragEnd, cancel: 'button, .no-drag' });
+  const height = props.height || 500;
 
   return (
-    <div 
-      ref={ref}
-      style={{ top, left, width, height }}
-      className={className ? `p6o-draggable-panel ${className}` : 'p6o-draggable-panel'}>
-      {children}
-    </div>
+    <Draggable cancel=".react-resizable-handle">
+      <ResizableBox 
+        className={className ? `p6o-draggable-panel ${className}` : 'p6o-draggable-panel'} 
+        style={{ top, left }}
+        height={height} 
+        width={width}>
+        {children}
+      </ResizableBox>
+    </Draggable>
   )
 
 }
