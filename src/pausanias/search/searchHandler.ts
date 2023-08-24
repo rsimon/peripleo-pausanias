127a447ad110d58003b7010724cec30614dd4c98
Rainer Simon <rainer@rainersimon.io>
2023-08-24 07:54:29 +0200

    Added missing feature: filtering the map by tag

--- src/pausanias/search/searchHandler.ts
+++ src/pausanias/search/searchHandler.ts
@@ -23,26 +23,45 @@ const aggregateByPlace = (items: Item<PlaceReference>[]) => {
 export const onSearch = (
   { args, store }: { args: SearchArgs, store: Store<PlaceReference> }
 ): SearchResult<Item<PlaceReference>> => {
-  const filter = args.filters?.find(f => f.name === 'visible-waypoints');
+  const viewportFilter = args.filters?.find(f => f.name === 'visible-waypoints');
+  const tagFilter = args.filters?.find(f => f.name === 'tag'); 
 
-  if (filter) {
-    const visibleReferences: Item<PlaceReference>[] = filter.value
-      .map((id: string) => store.getItemById(id))
-      // Import discareds references with no resolved place,
-      // therefore some can be undefined
-      .filter((item: Item<PlaceReference>) => item);
+  if (viewportFilter || tagFilter) {
+    const visibleReferences: Item<PlaceReference>[] = 
+      viewportFilter ? 
+        viewportFilter.value
+          .map((id: string) => store.getItemById(id))
+          // Import discareds references with no resolved place,
+          // therefore some can be undefined
+          .filter((item: Item<PlaceReference>) => item) :
 
-    const byPlace = aggregateByPlace(visibleReferences);
+        store
+          .allItems()
+          .filter((item: Item<PlaceReference>) => item);
+
+    const filteredByTag = tagFilter ? 
+      visibleReferences
+        .filter((item: Item<PlaceReference>) => { 
+          const p = item.target.value;
+          return p.tags.includes(tagFilter.value)
+        }) :
+
+      visibleReferences;
+        
+
+    const byPlace = aggregateByPlace(filteredByTag);
 
     return {
       // TODO doesn't take visible refs into account!
       bounds: store.getExtent(),
-      total: visibleReferences.length,
-      items: visibleReferences,
+      total: filteredByTag.length,
+      items: filteredByTag,
       aggregations: { byPlace }
     }
   } else {
-    const all = store.allItems();
+    const all = store
+      .allItems()
+      .filter((item: Item<PlaceReference>) => item);
 
     const byPlace = aggregateByPlace(all);
 
